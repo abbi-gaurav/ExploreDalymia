2d25b6060efcb5facbab0f4b67614e9dd38f6d2c
abbi-gaurav <abbi.gaurav@gmail.com>
2016-02-24 19:39:20 +0100

    added travis file

--- test/com/learn/test/SampleTest.scala
+++ test/com/learn/test/SampleTest.scala
@@ -3,10 +3,10 @@ package com.learn.test
 /**
   * Created by gabbi on 13/02/16.
   */
-class SampleTest extends BaseSpec{
+class SampleTest extends BaseSpec {
   "A Stack" must {
     "folllow FILO order" in {
-
+      "a".length must be(1)
     }
   }
 }
